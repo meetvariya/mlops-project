b15f1507da462374931bfb47242295c9bb43b830
Meet Variya <meetvariya00@gmail.com>
2026-04-12 17:09:16 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name="us_visa",
+    version="0.0.0",
+    author="Meet",
+    author_email="meetvariya29@gmail.com",
+    packages=find_packages()
+)
